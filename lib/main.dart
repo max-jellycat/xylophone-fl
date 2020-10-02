2397a5e5214a5bc7017f9341f7218a96f51e7ef1
max-jellycat <max@jellycat.fr>
2020-10-02 14:48:02 +0200

    refacto keys

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,24 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
+class Key {
+  int sound;
+  String note;
+  Color color;
+
+  Key(this.sound, this.note, this.color);
+}
+
+List<Key> keys = [
+  Key(1, "C", Colors.red),
+  Key(2, "D", Colors.orange),
+  Key(3, "E", Colors.yellow),
+  Key(4, "F", Colors.green),
+  Key(5, "G", Colors.teal),
+  Key(6, "A", Colors.blue),
+  Key(7, "B", Colors.purple),
+];
+
 void main() {
   runApp(
     MaterialApp(
@@ -23,11 +41,11 @@ class SoundController {
   static AudioPlayer audioPlayer = AudioPlayer();
   static AudioCache audioCache = AudioCache();
 
-  static void play(String sound) {
+  static void play(int sound) {
     if (Platform.isIOS) {
       audioPlayer.monitorNotificationStateChanges(audioPlayerHandler);
     }
-    audioCache.play(sound);
+    audioCache.play('note$sound.wav');
   }
 }
 
@@ -37,60 +55,29 @@ class XylyphonePage extends StatefulWidget {
 }
 
 class _XylyphonePageState extends State<XylyphonePage> {
+  Widget buildKey(int sound, String note, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () => SoundController.play(sound),
+        child: Text(
+          note,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Expanded(
-          child: FlatButton(
-            color: Colors.red,
-            onPressed: () => SoundController.play('note1.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.orange,
-            onPressed: () => SoundController.play('note2.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.yellow,
-            onPressed: () => SoundController.play('note3.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.green,
-            onPressed: () => SoundController.play('note4.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.teal,
-            onPressed: () => SoundController.play('note5.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.blue,
-            onPressed: () => SoundController.play('note6.wav'),
-            child: null,
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            color: Colors.purple,
-            onPressed: () => SoundController.play('note7.wav'),
-            child: null,
-          ),
-        ),
+        for (var key in keys) buildKey(key.sound, key.note, key.color)
       ],
     );
   }
