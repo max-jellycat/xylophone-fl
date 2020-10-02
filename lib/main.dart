6b5a700cb6919b31ab05b2c27347438c4095036b
max-jellycat <max@jellycat.fr>
2020-10-02 13:36:42 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: XylyphonePage(),
+      ),
+    ),
+  );
+}
+
+class XylyphonePage extends StatefulWidget {
+  @override
+  _XylyphonePageState createState() => _XylyphonePageState();
+}
+
+class _XylyphonePageState extends State<XylyphonePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
