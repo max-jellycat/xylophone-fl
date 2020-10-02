f208315b15a573b8c8dc63bcb1893ec5e7b1a9d4
max-jellycat <max@jellycat.fr>
2020-10-02 14:28:03 +0200

    play sounds on press

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        body: XylyphonePage(),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: XylyphonePage(),
+        ),
       ),
     ),
   );
+}
+
+void audioPlayerHandler(AudioPlayerState value) => print('state => $value');
+
+class SoundController {
+  static AudioPlayer audioPlayer = AudioPlayer();
+  static AudioCache audioCache = AudioCache();
+
+  static void play(String sound) {
+    if (Platform.isIOS) {
+      audioPlayer.monitorNotificationStateChanges(audioPlayerHandler);
+    }
+    audioCache.play(sound);
+  }
 }
 
 class XylyphonePage extends StatefulWidget {
@@ -19,6 +39,59 @@ class XylyphonePage extends StatefulWidget {
 class _XylyphonePageState extends State<XylyphonePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            color: Colors.red,
+            onPressed: () => SoundController.play('note1.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.orange,
+            onPressed: () => SoundController.play('note2.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.yellow,
+            onPressed: () => SoundController.play('note3.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.green,
+            onPressed: () => SoundController.play('note4.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.teal,
+            onPressed: () => SoundController.play('note5.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.blue,
+            onPressed: () => SoundController.play('note6.wav'),
+            child: null,
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            color: Colors.purple,
+            onPressed: () => SoundController.play('note7.wav'),
+            child: null,
+          ),
+        ),
+      ],
+    );
   }
 }
